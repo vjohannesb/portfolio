af00ab3e3ccbd8c97b0d4f843cf5a9c5896e7f3d
vjohannesb <johannes.b.nilsson@gmail.com>
2021-10-12 10:24:34 +0200

    update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import ContextProvider from "./components/context/ContextProvider";
 
 import "./index.scss";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ContextProvider>
+            <App />
+        </ContextProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
