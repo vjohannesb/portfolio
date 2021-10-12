7a96bb1c76fcae922c728f4ffdac74db529df424
vjohannesb <johannes.b.nilsson@gmail.com>
2021-10-12 10:22:08 +0200

    navigation v1

--- src/components/navigation/MainNav.tsx
+++ src/components/navigation/MainNav.tsx
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
+import { useAppContext } from "../context/ContextProvider";
 
 import "./MainNav.scss";
 
 const MainNav = (): JSX.Element => {
+    const [hoveringTheme, setHoveringTheme] = useState(false);
+
+    const {
+        darkThemeState: { darkTheme, setDarkTheme },
+    } = useAppContext();
+
+    const switchTheme = () => {
+        setDarkTheme((v) => !v);
+    };
+
     return (
-        <header id="navContainer">
-            <nav id="mainNavigation" className="container">
-                <ul>
-                    <li>Home</li>
-                    <li>Projects</li>
-                    <li>Resume</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header id="navContainer">
+                <nav id="mainNavigation" className="container">
+                    <ul>
+                        <li>
+                            <NavLink exact to="/">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to="/about">
+                                About
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to="/projects">
+                                Projects
+                            </NavLink>
+                        </li>
+                        <li
+                            className="theme-switch"
+                            onMouseEnter={() => setHoveringTheme(true)}
+                            onMouseLeave={() => setHoveringTheme(false)}
+                            onClick={switchTheme}>
+                            <SwitchTransition>
+                                <CSSTransition
+                                    key={darkTheme ? "Dark" : "Light"}
+                                    classNames="theme-anim"
+                                    timeout={400}>
+                                    {darkTheme ? (
+                                        <i className="far fa-moon"></i>
+                                    ) : (
+                                        <i className="far fa-sun"></i>
+                                    )}
+                                </CSSTransition>
+                            </SwitchTransition>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     );
 };
 
